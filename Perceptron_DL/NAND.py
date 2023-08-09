5ef626cb0335cf3b41b0ef585396b0c77b2d6a6d
Ashu <ashutosh.formin@gmail.com>
2023-08-09 22:40:02 +0800

    update

--- Perceptron_DL/NAND.py
+++ Perceptron_DL/NAND.py
@@ -1,26 +1,30 @@
+
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data
 import pandas as pd
 from utils.all_utils import save_model, save_plot
 
+def main(data , eta , epoch):
 
-NAND = {
-    "x1": [0,1,0,1],
-    "x2": [0,0,1,1],
-    "y": [1,1,1,0] 
-}
+   df = pd.DataFrame(NAND)
+   print(df)
+   X,y = prepare_data(df)
+   model = Perceptron(eta=ETA, epochs=EPOCHS)
+   model.fit(X, y)
 
-df = pd.DataFrame(NAND)
+   _ = model.total_loss()
+   save_model(model, "nand.model")
+   save_plot(df , "nand.png" , model)
 
-X,y = prepare_data(df)
+if __name__=="__main__":
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-save_model(model, "nand.model")
-save_plot(df , "nand.png" , model)
-
+    NAND = {
+        "x1": [0,1,0,1],
+        "x2": [0,0,1,1],
+        "y": [1,1,1,0] 
+    }
+     
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    main(data=NAND, eta=ETA, epoch= EPOCHS)
--- Perceptron_DL/and.py
+++ Perceptron_DL/and.py
@@ -1,26 +1,29 @@
+
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data
 import pandas as pd
 from utils.all_utils import save_model, save_plot
 
+def main(data,eta,epoch):
+    df = pd.DataFrame(AND)
+    print(df)
+    X,y = prepare_data(df)
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
 
-AND = {
+    _ = model.total_loss()
+    save_model(model, "and.model")
+    save_plot(df , "and.png" , model)
+
+if __name__=="__main__":
+
+    AND = {
     "x1": [0,1,0,1],
     "x2": [0,0,1,1],
     "y": [0,0,0,1] 
-}
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
 
-df = pd.DataFrame(AND)
-
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-save_model(model, "and.model")
-save_plot(df , "and.png" , model)
-
+    main(data=AND,eta=ETA,epoch=EPOCHS)
